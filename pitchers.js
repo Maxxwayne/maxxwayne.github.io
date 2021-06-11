2f1090c69e63e5cda357aad7558cf07762058483
Maxxwayne <61162622+Maxxwayne@users.noreply.github.com>
2021-06-10 19:33:28 -0500

    Update and rename pitchers.js to pitchers.html

--- pitchers.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript"><!--
- sr_share_id="rx3ME";
- BASE_URL = 'https://www.baseball-reference.com';
-  //-->
-</script>
-<script type="text/javascript"
-src="https://www.sports-reference.com/js/sr_share.js">
-</script>
-<noscript>Report from <a href="https://www.baseball-reference.com/">Baseball-Reference.com</a>.</noscript>
